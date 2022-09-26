52d8a46cceafc09ace2a2462c75aeb1fc8e99926
cse1940 <se1940@naver.com>
2022-09-26 18:52:34 +0900

    220926_upload

---
+++ src/Bronze/Q10430.java
@@ -0,0 +1,22 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class Q10430 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int c = s.nextInt();
+		
+		System.out.println((a+b) %c);
+		System.out.println(((a%c) + (b%c)) %c);
+		System.out.println((a*b) %c);
+		System.out.println(((a%c) * (b%c)) %c);
+
+	}
+
+}
---
+++ src/Bronze/Q2588.java
@@ -0,0 +1,22 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class Q2588 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		String b = s.next();
+		
+		System.out.println(a * (b.charAt(2) -'0'));
+		System.out.println(a * (b.charAt(1) -'0'));
+		System.out.println(a * (b.charAt(0) -'0'));
+		System.out.println(a * Integer.parseInt(b));
+		
+
+	}
+
+}
