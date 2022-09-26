407c95a05dfd9b2c42a156812e55828f45afbb3e
cse1940 <se1940@naver.com>
2022-09-26 18:02:34 +0900

    220926_upload

---
+++ src/Bronze/Q1001.java
@@ -0,0 +1,19 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class Q1001 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int c = a-b;
+		
+		System.out.println(c);
+
+	}
+
+}
---
+++ src/Bronze/Q2557.java
@@ -0,0 +1,11 @@
+package Bronze;
+
+public class Q2557 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World!");
+
+	}
+
+}
